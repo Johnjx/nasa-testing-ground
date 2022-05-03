8d35bfb68147a4caa3488df74f8c7b038072aed6
John Ruddock <john.jxr@pm.me>
2022-05-03 16:28:59 -0400

    initial project set up, installed deps

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        prim: '#FFF6DA',
+        sec: '#84F2D6',
+        ter: '#FC6B3F',
+        quad: '#262525',
+      }
+    },
+  },
+  plugins: [],
+}
