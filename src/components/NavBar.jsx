f0926bdf21c9b19e878514427545db223b7ac30c
John Ruddock <john.jxr@pm.me>
2022-05-07 20:37:00 -0400

    Nav Bar links have hover state now

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="h-12 bg-gray-200 dark:bg-slate-600 dark:text-white
-                         shadow-inner transition-all duration-300">
-            <nav className="flex justify-evenly">
-                <NavLink to="/">HOME</NavLink>
-                <NavLink to="/new-date">NEW DATE</NavLink>
+                         shadow-inner transition-all duration-300 flex justify-center items-center">
+            <nav className="w-screen flex justify-around">
+                <NavLink to="/" className="hover:text-ter dark:hover:text-teal-400">HOME</NavLink>
+                <NavLink to="/new-date" className="hover:text-ter dark:hover:text-teal-400">NEW DATE</NavLink>
             </nav>
         </div>
     )
