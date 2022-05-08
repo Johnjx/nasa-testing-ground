7cc56385ea9dc9e5380a860a9acbc74f93858e28
John Ruddock <john.jxr@pm.me>
2022-05-07 20:15:41 -0400

    Removed unused nav bar icons and related code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import { FaFire } from "react-icons/fa"
 
 const Navbar = () => {
     return (
@@ -8,23 +7,10 @@ const Navbar = () => {
             <nav className="flex justify-evenly">
                 <NavLink to="/">HOME</NavLink>
                 <NavLink to="/new-date">NEW DATE</NavLink>
-                <NavLink to="/"> <NavBarIcon icon={<FaFire size="30"/>} /> </NavLink>
-                <NavLink to="/"> <NavBarIcon icon={<FaFire size="30"/>} /> </NavLink>
             </nav>
         </div>
     )
 }
 
-const NavBarIcon = ({ icon, text="tooltip" }) => (
-    <div className="navbar-icon">
-        {icon}
-    </div>
-)
-
 export default Navbar;
 
-/**
- * <span className="navbar-tooltip">
-            {text}
-        </span>
- */
